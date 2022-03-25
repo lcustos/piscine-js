cfa959a357703f43c021aa3f47d8837fb85b1aef
custos <lucas.custos@ynov.com>
2022-03-25 16:20:22 +0100

    ex7

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -2,12 +2,12 @@ function first(x) {
     return x[0];
 }
 
-function last(x) {
-    return x[x.length];
+function last(x){
+    return x[x.length-1];
 }
 
 function kiss(x){
     let y = [];
-    y = x[0] + x[-1];
+    y = x[0] + x[x.length-1];
     return y;
 }
