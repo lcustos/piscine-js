87a8867033c1ccf3b64fca51282ae55ecc8a5a16
custos <lucas.custos@ynov.com>
2022-03-25 15:56:53 +0100

    ex4

--- mutability.js
+++ mutability.js
@@ -1,8 +1,17 @@
-let clone1 = person ;
+let clone1 = {
+    ...person,
+} ;
 
-let clone2 = person;
+let clone2 = {
+    ...clone1,
+};
 
-let samePerson = person;
+let samePerson = {
+    ...person,
+};
+
+Object.freeze(clone1)
+Object.freeze(clone2)
 
 person.age=person.age + 1
 person.country = 'FR'
