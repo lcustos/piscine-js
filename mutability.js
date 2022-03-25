639b15c9b789401f21ffe4c31a8805a11376d451
custos <lucas.custos@ynov.com>
2022-03-25 15:46:02 +0100

    ex4

--- mutability.js
+++ mutability.js
@@ -1,7 +1,12 @@
-const clone1 = person
-const clone2 = person
-const samePerson = person
-Object.freeze(samePerson)
+const clone1 = {
+    ...person
+}
 
-person.age+=1
-person.country='FR'
+const clone2 = {
+    ...person
+}
+
+const samePerson = {
+    ...person
+}
+Object.freeze(samePerson)
