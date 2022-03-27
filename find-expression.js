5ca85a8f08e82b13b35db9bd8d8193122128a1e8
custos <lucas.custos@ynov.com>
2022-03-27 20:00:13 +0200

    ex12

--- find-expression.js
+++ find-expression.js
@@ -2,22 +2,23 @@ const add4 = '+4'
 const mul2 = '*2'
 let expr = "1"
 let flag = false
+
 function findExpression(x) {
     if (x === 1) {
         return expr
-    } else if (x % 2 === 0) {
-        expr += ' ' + mul2
-        findExpression(x/2)
     } else if (x - 4 >= 0) {
         expr += ' ' + add4
-        findExpression(x-4)
+        findExpression(x - 4)
+    } else if (x % 2 === 0) {
+        expr += ' ' + mul2
+        findExpression(x / 2)
     } else {
         flag = true
     }
-    if (flag === true){
+    if (flag === true) {
         return undefined
 
-    }else{
+    } else {
         return expr
     }
 }
