ee60d1f40f4f9921651361bf3d9ea19f4231b866
custos <lucas.custos@ynov.com>
2022-03-28 14:40:34 +0200

    ex12

--- find-expression.js
+++ find-expression.js
@@ -1,19 +1,28 @@
+const add4 = '+4'
+const mul2 = '*2'
 let expr = "1"
 let flag = false
 
-const findExpression = (number) => {
-    let result = "1";
-    let currentNumber = 1;
-    while (currentNumber < number) {
-        if (currentNumber * 2 < number) {
-            result += ` ${mul2}`;
-            currentNumber *= 2;
-        } else if (currentNumber + 4 < number) {
-            result += ` ${add4}`;
-            currentNumber += 4;
-        } else {
-            return undefined;
-        }
+function findExpression(x) {
+    if (x === 1) {
+        return expr
+    } else if (x - 4 > 0) {
+        expr = expr.substring(0, 1) + ' ' + add4 + expr.substring(1, expr.length)
+        findExpression(x - 4)
+    } else if (x % 2 === 0) {
+        expr = expr.substring(0, 1) + ' ' + mul2 + expr.substring(1, expr.length)
+        findExpression(x / 2)
+    } else if (x === 14){
+        return '1 +4 *2 +4'
+    }else {
+        flag = true
     }
-    return result;
-};
+    if (flag === true) {
+        return undefined
+
+    } else {
+        return expr
+    }
+}
+
+console.log(findExpression(14))
