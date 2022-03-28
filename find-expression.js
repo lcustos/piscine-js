8cb6cc3fbfc898cb21565d99f16fe5efc440eb42
custos <lucas.custos@ynov.com>
2022-03-28 14:48:29 +0200

    ex12

--- find-expression.js
+++ find-expression.js
@@ -6,8 +6,6 @@ let flag = false
 function findExpression(x) {
     if (x === 1) {
         return expr
-    } else if (x === 14){
-        return '1 +4 *2 +4'
     } else if (x - 4 > 0) {
         expr = expr.substring(0, 1) + ' ' + add4 + expr.substring(1, expr.length)
         findExpression(x - 4)
