d283442095353b7f1813802b3159ef252e57a3c5
custos <lucas.custos@ynov.com>
2022-03-30 11:17:33 +0200

    ex12

---
+++ same-amount.js
@@ -0,0 +1,9 @@
+function sameAmount(str,reg1,reg2){
+    const regex1 = new RegExp(reg1,'g')
+    const regex2 = new RegExp(reg2,'g')
+    const resRegex1 = str.match(regex1)
+    const resRegex2 = str.match(regex2)
+    console.log(resRegex1,'---',resRegex2)
+    return (resRegex1 != null && resRegex2 != null) && (resRegex1.length === resRegex2.length);
+
+}
