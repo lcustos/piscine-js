42bf295a985e7cafebf7784deb48e95940dde7ca
custos <lucas.custos@ynov.com>
2022-04-01 14:00:13 +0200

    ex12

--- greedy-url.js
+++ greedy-url.js
@@ -1,13 +1,14 @@
 //getURL that returns an array with all valid URLs present in a data-set, http and https
 
-function getURL() {
-    var url = [];
-    var urlRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
-    var urlArray = data.match(urlRegex);
-    for (var i = 0; i < urlArray.length; i++) {
-        url.push(urlArray[i]);
+function getURL(dataset) {
+  var url = [];
+  var regex = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  for (var i = 0; i < dataset.length; i++) {
+    if (regex.test(dataset[i])) {
+      url.push(dataset[i]);
     }
-    return url;
+  }
+  return url;
 }
 
 //greedyQuery that returns URL with at least 3 or more parameters from all URLs that contain a query.
