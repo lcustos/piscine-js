986220c7ba36fff91f13f961140e698d45e1ea4c
custos <lucas.custos@ynov.com>
2022-04-05 13:24:22 +0200

    /

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,19 @@
+export function generateLetters(){
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for(let i=1;i<=120;i++){
+        let char = ''
+        char += possible.charAt(Math.floor(Math.random()*possible.length))
+        let elem = document.createElement('div')
+        elem.innerHTML = char
+        elem.style.fontSize = (i+10)+'px';
+        if (i <= 40 ){
+            elem.style.fontWeight = "300";
+        } else if (i <= 80) {
+            elem.style.fontWeight = "400";
+        } else {
+            elem.style.fontWeight = "600";
+        }
+        document.body.append(elem)
+        console.log(i)
+    }
+}
