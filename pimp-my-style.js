da34be9a8527cb63adc71b19bdfb6b5202c0c43d
custos <lucas.custos@ynov.com>
2022-04-15 13:40:33 +0200

    /

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -7,8 +7,6 @@ let k = 0
 export function pimp() {
     let elem = document.querySelector("button")
     let mat = Math.floor(i / styles.length)
-    console.log(elem.classList)
-    console.log(j)
     if (mat % 2 === 0) {
         j = 14
         elem.classList.add(styles[k])
